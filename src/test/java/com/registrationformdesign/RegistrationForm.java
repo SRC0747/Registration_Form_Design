bbc67767273d412d680a4eb70ffab6ddb2ac4f80
SRC0747 <RjSrc@0747>
2021-08-29 19:20:36 +0530

    Registration Form Design

---
+++ src/test/java/com/registrationformdesign/RegistrationForm.java
@@ -0,0 +1,7 @@
+package com.registrationformdesign;
+
+public class RegistrationForm {
+    public static void main(String[] args){
+        System.out.println("Registration Form Design");
+    }
+}
